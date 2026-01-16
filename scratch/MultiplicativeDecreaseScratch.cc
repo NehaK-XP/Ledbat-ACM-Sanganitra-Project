571e76aca0fa99415610f32d5e342e539290a377
Hanuman-S <hanuman212006@gmail.com>
2026-01-16 17:04:54 +0530

    Fixed LEDBAT initialisation

--- scratch/MultiplicativeDecreaseScratch.cc
+++ scratch/MultiplicativeDecreaseScratch.cc
@@ -12,47 +12,37 @@ NS_LOG_COMPONENT_DEFINE ("LedbatMdTest");
 int
 main (int argc, char *argv[])
 {
-  /* ================= LOGGING ================= */
   LogComponentEnable ("LedbatMdTest", LOG_LEVEL_INFO);
   LogComponentEnable ("TcpLedbat", LOG_LEVEL_ALL);
 
   NS_LOG_INFO ("LEDBAT + Cubic simulation started");
 
-  /* ================= NODES ================= */
   NodeContainer nodes;
-  nodes.Create (2);
+  nodes.Create (2); //Sender and Receiver
 
-  /* ================= LINK ================= */
-  PointToPointHelper p2p;
+  PointToPointHelper p2p; //Setting Channel bandwidth and delay, in a way to increase delay as much as possible
   p2p.SetDeviceAttribute ("DataRate", StringValue ("500Kbps"));
   p2p.SetChannelAttribute ("Delay", StringValue ("100ms"));
 
   NetDeviceContainer devices = p2p.Install (nodes);
 
-  /* ================= INTERNET STACK ================= */
   InternetStackHelper internet;
-  internet.Install (nodes);
+  internet.Install (nodes); // Adding TCP, IP, ... required protocols
 
-  /* ================= QUEUE DISC ================= */
   TrafficControlHelper tch;
   tch.SetRootQueueDisc ("ns3::PfifoFastQueueDisc",
-                        "MaxSize", StringValue ("10p"));
+                        "MaxSize", StringValue ("10p")); // 10 packets in one queue
   tch.Install (devices);
 
-  /* ================= IP ================= */
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
   Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
-  /* ================= TCP TYPES ================= */
   TypeId cubicTid  = TypeId::LookupByName ("ns3::TcpCubic");
   TypeId ledbatTid = TypeId::LookupByName ("ns3::TcpLedbat");
 
-  /* =================================================
-   * CUBIC FLOW (AGGRESSIVE, STARTS EARLY)
-   * ================================================= */
   Config::Set ("/NodeList/0/$ns3::TcpL4Protocol/SocketType",
-               TypeIdValue (cubicTid));
+               TypeIdValue (cubicTid)); // Cubic initialisation
 
   uint16_t cubicPort = 6000;
 
@@ -72,11 +62,8 @@ main (int argc, char *argv[])
 
   cubicSink.Install (nodes.Get (1));
 
-  /* =================================================
-   * LEDBAT FLOW (DELAY-BASED, STARTS LATE)
-   * ================================================= */
   Config::Set ("/NodeList/0/$ns3::TcpL4Protocol/SocketType",
-               TypeIdValue (ledbatTid));
+               TypeIdValue (ledbatTid)); // LEDBAT initialisation
 
   uint16_t ledbatPort = 7000;
 
@@ -96,7 +83,6 @@ main (int argc, char *argv[])
 
   ledbatSink.Install (nodes.Get (1));
 
-  /* ================= RUN ================= */
   Simulator::Stop (Seconds (10.0));
   Simulator::Run ();
   Simulator::Destroy ();
